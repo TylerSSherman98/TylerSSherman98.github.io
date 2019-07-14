0cb9a61b060e6905e16087826ee827941489c3a1
Tyler Seiden Sherman <TylerSSherman98@gmail.com>
2019-07-14 19:17:30 -0400

    Complete overhual for 2019-2020 application cycle
    - Security patches
    - Compressed images
    - Minify css
    - Redo "skills" section

---
+++ js/barChart.js
@@ -0,0 +1,52 @@
+var yLabels = {
+	  0 : '',
+    1 : 'Basic',
+    2 : 'Novice',
+    3 : 'Intermediate',
+    4 : 'Advanced',
+    5 : 'Expert',
+}
+var data = [2.5, 3, 3.5, 4, 2.5, 3, 2.5, 1];
+
+var bar = document.getElementById("barChart").getContext('2d');
+var myChart = new Chart(bar, {
+    type: 'horizontalBar',
+    data: {
+        labels: ['C', 'HTML & CSS', 'Java', 'MATLAB', 'Python', 'SQL', 'SystemVerilog', 'Swift'],
+        datasets: [{
+            data: data,
+            backgroundColor: 'rgba(68, 108, 179, 1)',
+        }]
+    },
+    options: {
+        responsive: true,
+        title: {
+          display: false,
+        },
+        legend: {
+          display: false
+        },
+        tooltips: {
+          enabled: false
+        },
+        scales: {
+            xAxes: [{
+              ticks: {
+                    beginAtZero: true,
+                    callback: function(value, _index, _values) {
+                        return yLabels[value];
+                    },
+                    maxRotation: 0,
+                    minRotation: 0,
+                    fontSize: 14,
+                    max: 5
+                }
+            }],
+            yAxes: [{
+              ticks: {
+                    fontSize: 14,
+                }
+            }]
+        }
+    }
+});
