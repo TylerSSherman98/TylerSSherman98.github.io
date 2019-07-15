eacbc8ee0519a2533233b4f7a6be941c24d91a17
Tyler Seiden Sherman <TylerSSherman98@gmail.com>
2019-07-14 20:04:40 -0400

    Bar testing

--- js/barChart.js
+++ js/barChart.js
@@ -12,7 +12,7 @@ var bar = document.getElementById("barChart").getContext('2d');
 var myChart = new Chart(bar, {
     type: 'horizontalBar',
     data: {
-        labels: ['C1', 'HTML & CSS', 'Java', 'MATLAB', 'Python', 'SQL', 'SystemVerilog', 'Swift'],
+        labels: ['C', 'HTML & CSS', 'Java', 'MATLAB', 'Python', 'SQL', 'SystemVerilog', 'Swift'],
         datasets: [{
             data: data,
             backgroundColor: 'rgba(68, 108, 179, 1)',
@@ -37,17 +37,17 @@ var myChart = new Chart(bar, {
                     callback: function(value, _index, _values) {
                         return yLabels[value];
                     },
-                    fontSize: 12,
+                    fontSize: 13,
 										min: 0,
                     max: 5,
 										autoSkip: false,
-										minRotation: 90,
-										maxRotation: 90,
+										minRotation: 70,
+										maxRotation: 70,
                 }
             }],
             yAxes: [{
               ticks: {
-                    fontSize: 12,
+                    fontSize: 13,
 										autoSkip: false,
 										minRotation: 40,
 										maxRotation: 40,
